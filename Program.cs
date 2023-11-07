309f2ed90edb2473019a2283b1f44d72e8a0954f
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2023-11-07 18:38:49 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -1,123 +1,107 @@
-﻿namespace Numberguess
+﻿using System;
+
+namespace NumberGuess
 {
     internal class Program
     {
-        static void StartGame()
+        static Random random = new Random();
+
+        static int GenerateRandomNumber(int min, int max)
         {
-            bool IsCorrectGuess = false;
-            int Flag = 0;
-            Random RandomInt = new Random();
+            return random.Next(min, max + 1);
+        }
 
-            int RandomNum = RandomInt.Next(1, 11);
-            Console.WriteLine("Let's play the game ");
-            Console.WriteLine("Guess the number between 1 to 10 friends \nyou have 3 guess");
-
-
-            int ExtraLifeFlag = 0;
-
-            while (!IsCorrectGuess && Flag <= 3 && ExtraLifeFlag == 0)
+        static int GetGuess()
+        {
+            int guess;
+            while (true)
             {
-
-                try
+                Console.WriteLine("Enter your guess:");
+                if (int.TryParse(Console.ReadLine(), out guess))
                 {
-                    if (Flag == 3)
-                    {
-                        Console.WriteLine("Don't worry restart the game \n->A - Add extra 1 guess \n->Any Key  - Exit");
-                        string Choice = " ";
-                        Choice = Console.ReadLine().ToLower();
-                        if (Choice == "a" && ExtraLifeFlag<=1)
-                        {
-                            Flag--;
-                            ExtraLifeFlag++;
-                            break;
-                        }
-                        else
-                        {
-                            EndGame();
-                            break;
-                        }
-                    }
-                    Console.WriteLine("Enter your guess");
-                    int guess = Convert.ToInt32(Console.ReadLine());
+                    return guess;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
+        }
 
-                    if (guess == RandomNum)
+        static int CalculateScore(int attempts)
+        {
+            if (attempts == 3)
+            {
+                return 20;
+            }
+            else if (attempts == 2)
+            {
+                return 15;
+            }
+            else if (attempts == 1)
+            {
+                return 10;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static void EndGame(int score, int num)
+        {
+            if (score > 0)
+            {
+                Console.WriteLine($"Congratulations! {num} is the right answer, You got {score} points!");
+            }
+            else
+            {
+                Console.WriteLine($"Better luck next time! {num} is not right");
+            }
+            Console.WriteLine("Press Enter to start a new game...");
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int attempts = 3;
+                int guess;
+
+                Console.Clear();
+                Console.WriteLine("Let's play the game ");
+                Console.WriteLine("Guess the number between 1 to 10 friends \nyou have 3 guesses");
+
+                int randomNumber = GenerateRandomNumber(1, 10);
+
+                while (attempts > 0)
+                {
+                    guess = GetGuess();
+
+                    if (guess == randomNumber)
                     {
-                        if (Flag == 0)
-                        {
-                            Console.WriteLine("Whoo hoo! 20 points");
-                        }else if(Flag==1){
-                            Console.WriteLine("Awesome! 15 points");
-                        }
-                        else if (Flag == 2 && ExtraLifeFlag==1) {
-                            Console.WriteLine("Hoo! you got 5 points");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Perfect ! You got 10 points");
-                        }
-                        IsCorrectGuess = true;
+                        int score = CalculateScore(attempts);
+                        EndGame(score, guess);
                         break;
                     }
-                    else if (guess > RandomNum)
+                    else if (guess > randomNumber)
                     {
                         Console.WriteLine("I am appreciating you are thinking big but not now ");
-                        Flag++;
                     }
                     else
                     {
                         Console.WriteLine("Think big ");
-                        Flag++;
                     }
+
+                    attempts--;
                 }
-                catch (Exception e)
+
+                if (attempts == 0)
                 {
-
-                    Console.WriteLine(e.Message);
+                    EndGame(0, randomNumber);
                 }
-
-
-
             }
-            Console.WriteLine("Enter to Continue");
-            Console.ReadKey();
-            Console.Clear();
-            
-
-        }
-        static void EndGame()
-        {
-            Console.Clear();
-            Console.WriteLine("Thanks for playing!");
-            Console.WriteLine("Enter to new game.....");
-            Console.ReadKey();
-
-        }
-        static void Main(string[] args)
-        {
-            
-
-            StartGame();
-            
-            Console.Clear();
-            Console.WriteLine("Enter the key \n->N-New game \n->Any key-To Exit");
-            string UserChoice = Console.ReadLine().ToLower();
-            switch(UserChoice)
-            {
-                case "n":
-                    {
-                        Console.Clear();
-                        StartGame();
-                        break;
-                    }
-                default:
-                    {
-                        EndGame();
-                        break;
-                    }
-            }
-            
-            Console.ReadKey();
-
         }
     }
 }
